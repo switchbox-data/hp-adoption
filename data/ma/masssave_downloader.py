e085a11db214abd59cd2b748b351566e8f5b6c4a
karpdave <dave.karp@gmail.com>
2025-07-01 21:58:53 +0000

    lint

--- data/ma/masssave_downloader.py
+++ data/ma/masssave_downloader.py
@@ -15,6 +15,7 @@
 
 
 import asyncio
+import itertools
 import json
 from datetime import datetime
 from typing import Any
@@ -42,33 +43,40 @@ SHARED_HEADERS = {
     "x-powerbi-hostenv": "Embed for Customers",
 }
 
-COLUMNS_TO_SOURCES = {
-    "Suppression option": "s",
-    "Year": "d",
-    "Heat_pump": "d1",
-    "YEAR": "a",
-    "Sector": "d21",
-    "Is_track_new_meas": "f",
-    "Suppression status": "s1",
-    "Option": "s2",
-    "New_Construction_Rule": "n",
-    "Suppression_visible": "s3",
+# COLUMNS_TO_SOURCES = {
+#     "Suppression option": "s",
+#     "YEAR": "a",
+#     "Is_track_new_meas": "f",
+#     "Suppression status": "s1",
+#     "Option": "s2",
+#     "New_Construction_Rule": "n",
+#     "Suppression_visible": "s3",
+# }
+
+FILTERS_TO_SELECTORS = {
+    "End use": "Dim_End_use",
+    "Rate_category": "Dim_Rate_Category",
+    "Displaced_fuel": "Dim_Displaced_fuel",
+    "Year": "Dim_Year",
 }
 OPERATORS = ["In", "Less_than", "Greater_than"]
 
 
 @define()
 class MassSaveFilter:
-    column: str = field(validator=in_(COLUMNS_TO_SOURCES.keys()))
+    column: str = field(validator=in_(FILTERS_TO_SELECTORS.keys()))
     values: list[str] = field()
     operator: str = field(default="In", validator=in_(OPERATORS))
     invert: bool = field(default=False)
 
     @classmethod
     def show_columns(cls) -> list[str]:
-        return list(COLUMNS_TO_SOURCES.keys())  # todo, this should be stored in a nicer way
+        return list(FILTERS_TO_SELECTORS.keys())
 
-    def to_dict(self) -> dict:
+    def selector_column(self) -> str:
+        return FILTERS_TO_SELECTORS[self.column]
+
+    def to_dict(self, source_ref: str) -> dict:
         match self.operator:
             case "In":
                 cond = {
@@ -76,7 +84,7 @@ class MassSaveFilter:
                         "Expressions": [
                             {
                                 "Column": {
-                                    "Expression": {"SourceRef": {"Source": COLUMNS_TO_SOURCES[self.column]}},
+                                    "Expression": {"SourceRef": {"Source": source_ref}},
                                     "Property": self.column,
                                 }
                             }
@@ -91,7 +99,7 @@ class MassSaveFilter:
                         "ComparisonKind": 2,
                         "Left": {
                             "Column": {
-                                "Expression": {"SourceRef": {"Source": COLUMNS_TO_SOURCES[self.column]}},
+                                "Expression": {"SourceRef": {"Source": source_ref}},
                                 "Property": self.column,
                             }
                         },
@@ -106,7 +114,7 @@ class MassSaveFilter:
                         "Left": {"Literal": {"Value": self.values[0]}},
                         "Right": {
                             "Column": {
-                                "Expression": {"SourceRef": {"Source": COLUMNS_TO_SOURCES[self.column]}},
+                                "Expression": {"SourceRef": {"Source": source_ref}},
                                 "Property": self.column,
                             }
                         },
@@ -139,21 +147,21 @@ class MassSaveQuery:
         query = {
             "Version": 2,
             "From": [
-                {"Name": "d2", "Entity": "Dim_City", "Type": 0},
                 {"Name": "f", "Entity": "Fact", "Type": 0},
                 {"Name": "s", "Entity": "Suppression table", "Type": 0},
-                {"Name": "d", "Entity": "Dim_Year", "Type": 0},
-                {"Name": "d1", "Entity": "Dim_Heat_pump", "Type": 0},
                 {"Name": "a", "Entity": "ACS", "Type": 0},
-                {"Name": "d21", "Entity": "Dim_Sector", "Type": 0},
                 {"Name": "s1", "Entity": "Suppression global switch", "Type": 0},
                 {"Name": "s2", "Entity": "Suppression style", "Type": 0},
                 {"Name": "n", "Entity": "New Construction Rule", "Type": 0},
                 {"Name": "s3", "Entity": "Suppression display control", "Type": 0},
-            ],
+                {"Name": "c", "Entity": "Dim_City", "Type": 0},
+                {"Name": "h", "Entity": "Dim_Heat_pump", "Type": 0},
+                {"Name": "s4", "Entity": "Dim_Sector", "Type": 0},
+            ]
+            + [{"Name": f"d{i}", "Entity": f.selector_column(), "Type": 0} for i, f in enumerate(self.filters)],
             "Select": [
                 {
-                    "Column": {"Expression": {"SourceRef": {"Source": "d2"}}, "Property": "City"},
+                    "Column": {"Expression": {"SourceRef": {"Source": "c"}}, "Property": "City"},
                     "Name": "Dim_City.City",
                     "NativeReferenceName": "Municipality",
                 },
@@ -168,11 +176,153 @@ class MassSaveQuery:
                     "NativeReferenceName": "Installed heat pumps (locations)",
                 },
             ],
-            "Where": [f.to_dict() for f in self.filters] if self.filters is not None else [],
+            "Where": [
+                {
+                    "Condition": {
+                        "In": {
+                            "Expressions": [
+                                {
+                                    "Column": {
+                                        "Expression": {"SourceRef": {"Source": "s"}},
+                                        "Property": "Suppression option",
+                                    }
+                                }
+                            ],
+                            "Values": [[{"Literal": {"Value": "'City'"}}]],
+                        }
+                    }
+                },
+                # {"Condition": {"In": {"Expressions": [{"Column": {"Expression": {"SourceRef": {"Source": "d"}}, "Property": "End use"}}], "Values": [[{"Literal": {"Value": "'HVAC'"}}]]}}},
+                # {"Condition": {"In": {"Expressions": [{"Column": {"Expression": {"SourceRef": {"Source": "d1"}}, "Property": "Year"}}], "Values": [[{"Literal": {"Value": "2019L"}}]]}}},
+                {
+                    "Condition": {
+                        "Not": {
+                            "Expression": {
+                                "In": {
+                                    "Expressions": [
+                                        {
+                                            "Column": {
+                                                "Expression": {"SourceRef": {"Source": "h"}},
+                                                "Property": "Heat_pump",
+                                            }
+                                        }
+                                    ],
+                                    "Values": [[{"Literal": {"Value": "'Energy efficiency'"}}]],
+                                }
+                            }
+                        }
+                    }
+                },
+                # {"Condition": {"Comparison": {"ComparisonKind": 2, "Left": {"Column": {"Expression": {"SourceRef": {"Source": "a"}}, "Property": "YEAR"}}, "Right": {"Literal": {"Value": "2019L"}}}}},
+                {
+                    "Condition": {
+                        "In": {
+                            "Expressions": [
+                                {"Column": {"Expression": {"SourceRef": {"Source": "s4"}}, "Property": "Sector"}}
+                            ],
+                            "Values": [[{"Literal": {"Value": "'Residential'"}}]],
+                        }
+                    }
+                },
+                {
+                    "Condition": {
+                        "In": {
+                            "Expressions": [
+                                {
+                                    "Column": {
+                                        "Expression": {"SourceRef": {"Source": "f"}},
+                                        "Property": "Is_track_new_meas",
+                                    }
+                                }
+                            ],
+                            "Values": [[{"Literal": {"Value": "1L"}}]],
+                        }
+                    }
+                },
+                {
+                    "Condition": {
+                        "Not": {
+                            "Expression": {
+                                "In": {
+                                    "Expressions": [
+                                        {
+                                            "Column": {
+                                                "Expression": {"SourceRef": {"Source": "h"}},
+                                                "Property": "Heat_pump",
+                                            }
+                                        }
+                                    ],
+                                    "Values": [[{"Literal": {"Value": "'No heat pump'"}}]],
+                                }
+                            }
+                        }
+                    }
+                },
+                ## IMPORTANT: Suppression status is OFF
+                {
+                    "Condition": {
+                        "In": {
+                            "Expressions": [
+                                {
+                                    "Column": {
+                                        "Expression": {"SourceRef": {"Source": "s1"}},
+                                        "Property": "Suppression status",
+                                    }
+                                }
+                            ],
+                            "Values": [[{"Literal": {"Value": "'Suppression OFF'"}}]],
+                        }
+                    }
+                },
+                {
+                    "Condition": {
+                        "In": {
+                            "Expressions": [
+                                {"Column": {"Expression": {"SourceRef": {"Source": "s2"}}, "Property": "Option"}}
+                            ],
+                            "Values": [[{"Literal": {"Value": "'*'"}}]],
+                        }
+                    }
+                },
+                {
+                    "Condition": {
+                        "In": {
+                            "Expressions": [
+                                {
+                                    "Column": {
+                                        "Expression": {"SourceRef": {"Source": "n"}},
+                                        "Property": "New_Construction_Rule",
+                                    }
+                                }
+                            ],
+                            "Values": [[{"Literal": {"Value": "true"}}]],
+                        }
+                    }
+                },
+                # This could maybe flip to False? It doesn't appear to affect the results
+                {
+                    "Condition": {
+                        "In": {
+                            "Expressions": [
+                                {
+                                    "Column": {
+                                        "Expression": {"SourceRef": {"Source": "s3"}},
+                                        "Property": "Suppression_visible",
+                                    }
+                                }
+                            ],
+                            "Values": [[{"Literal": {"Value": "false"}}]],
+                        }
+                    }
+                },
+            ]
+            + [f.to_dict(f"d{i}") for i, f in enumerate(self.filters)]
+            if self.filters is not None
+            else [],
             "OrderBy": [
                 {
                     "Direction": 1,
-                    "Expression": {"Column": {"Expression": {"SourceRef": {"Source": "d2"}}, "Property": "City"}},
+                    "Expression": {"Column": {"Expression": {"SourceRef": {"Source": "c"}}, "Property": "City"}},
                 }
             ],
         }
@@ -211,6 +361,9 @@ class MassSaveQuery:
 
     @staticmethod
     def _json_to_df(data: dict) -> pl.DataFrame:
+        # TODO: Maybe also just store the "Total" number, which is in
+        # data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][1]["DM0"]
+
         data_array = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][1]["DM1"]
 
         # Create a list of dictionaries for each city
@@ -226,23 +379,28 @@ class MassSaveQuery:
         # Create DataFrame - keep municipality as a regular column
         return pl.DataFrame(rows)
 
-    def run_query(self, token: str, return_type: str = "df") -> dict | pl.DataFrame:
+    def run_query_dict(self, token: str) -> dict:
         headers = SHARED_HEADERS | {"authorization": f"EmbedToken {token}"}
 
         # Create and send the query
         query_payload = self._create_query()
+        # print(json.dumps(query_payload, indent=4)) # debug line
 
         response = requests.post(self.endpoint_url, headers=headers, json=query_payload, timeout=30)
 
         if response.status_code == 200:
             content = response.text.encode("utf-8").decode("utf-8-sig")  # Remove BOM
             data = json.loads(content)
-            return self.__class__._json_to_df(data) if return_type == "df" else data
+            # print(json.dumps(data, indent=4)) # debug line
+            return data
         else:
             # todo, this should be a proper error
             print(f"Error: {response.status_code}")
             print(response.text)
             return {}
+
+    def run_query(self, token: str) -> pl.DataFrame:
+        return self.__class__._json_to_df(self.run_query_dict(token))
 
 
 async def extract_auth_token():
@@ -309,12 +467,38 @@ if __name__ == "__main__":
     date = datetime.now().strftime("%Y%m%d")
     outfile = f"masssave_hpinstalls_{date}.csv"
 
-    # TODO: add a way to get the token from the user
-    msq = MassSaveQuery(filters=[MassSaveFilter(column="Heat_pump", values=["Energy efficiency"], operator="In")])
+    filter_sets = {
+        "Year": ["2019", "2020", "2021", "2022", "2023"],
+        "Displaced_fuel": ["No displacement", "Electric", "Gas", "Oil", "Propane", "Other"],
+        "End use": ["Hot Water", "HVAC"],
+        "Rate_category": ["Market rate", "Income eligible"],
+    }
+    filter_sets = {
+        "Year": ["2019"],
+    }
+
+    filter_cols = [s.lower().replace(" ", "_") for s in filter_sets]
+
+    query_filters = []
+    for col, vals in filter_sets.items():
+        query_filters.append([MassSaveFilter(column=col, values=[val], operator="In") for val in vals])
 
     auth_token = asyncio.run(extract_auth_token())
 
-    # TODO: run all permutations of filters and concat
-    data = msq.run_query(auth_token)
+    dfs = []
+    for filter_combos in itertools.product(*query_filters):
+        msq = MassSaveQuery(filters=list(filter_combos))
+        # print(msq.filters) # debug line
+        df = msq.run_query(auth_token)
+        if df.is_empty():
+            print(f"No data found for filters: {filter_combos}")
+            continue
+        else:
+            df = df.with_columns(*[
+                pl.lit(f.values[0]).alias(f.column.lower().replace(" ", "_")) for f in filter_combos
+            ])
+            # print(msq.filters,df["installed_hp_accounts"].sum()) # debug line
+            dfs.append(df)
 
+    data = pl.concat(dfs).sort(filter_cols).select(*[pl.col(s) for s in filter_cols], pl.all().exclude(filter_cols))
     data.write_csv(outfile)
